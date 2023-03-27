a51d38ebdb608da580df4fa9f236d49623aaf4df
Vasiliy38 <109076846+Vasiliy38@users.noreply.github.com>
2023-03-27 18:32:52 +1100

    Add files via upload

---
+++ region35.js
@@ -0,0 +1,37 @@
+ymaps.ready(function () {
+    const searchControls = new ymaps.control.SearchControl({
+        options: {
+            float: 'right',
+            noPlacemark: true
+        }
+    })
+
+    var myMap = new ymaps.Map("map35", {
+        center: [59.220496, 39.891523],
+        zoom: 6,
+        controls: [searchControls]
+    })
+    const removeControls = [
+        'geolocationControl',
+        'trafficControl',
+        'fullscreenControl',
+        'zoomControl', 'rulerControl',
+        'typeSelector'
+    ]
+
+    const clearTheMap = myMap => {
+        removeControls
+            .forEach(controls => myMap.controls.remove(controls))
+    }
+
+    clearTheMap(myMap)
+
+    fetch('regions/35.json',)
+        .then(arr => arr.json())
+        .then(arr => {
+            var objectManager = new ymaps.ObjectManager({clusterize: false});
+
+            objectManager.add(arr)
+            myMap.geoObjects.add(objectManager)
+        })
+})
